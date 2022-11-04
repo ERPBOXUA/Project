3956525d0b695603e8d0012af4d0bc2ac0a74f7b
dvakiev <39129598+dvakiev@users.noreply.github.com>
2022-11-04 18:57:34 +0200

    sale_expense_manual_reinvoice

--- sale_expense_manual_reinvoice/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import account_analytic_line
-from . import account_move_line
-from . import hr_expense
-from . import product_template
--- sale_expense_manual_reinvoice/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_sale_expense_manual_reinvoice
